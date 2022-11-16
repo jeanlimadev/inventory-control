8071ec2eb0f7af6cccf9d9a9f854ad44d1242ab1
Jean Lima <jeanlima.dev@gmail.com>
2022-11-16 12:04:48 -0300

    refact: show available stock on list products route

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "4eab562f7972adc092404fae42d88943",
-  expires_in_token: "30m",
+  expires_in_token: "1d",
   secret_refresh_token: "b96baaa79b57e03a6227f91f8fca19b8",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
--- src/controllers/products/ListProducts/ListProductsController.ts
+++ src/controllers/products/ListProducts/ListProductsController.ts
@@ -6,6 +6,32 @@ class ListProductsController {
     try {
       const products = await prismaClient.product.findMany();
 
+      for (let i = 0; i < products.length; i++) {
+        const purchases = await prismaClient.buy_products.aggregate({
+          where: {
+            product_id: products[i].id,
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+        const sales = await prismaClient.sell_products.aggregate({
+          where: {
+            product_id: products[i].id,
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+        const calculateInventory = {
+          inventory: Number(purchases._sum.amount) - Number(sales._sum.amount),
+        };
+
+        Object.assign(products[i], calculateInventory);
+      }
+
       return response.json(products);
     } catch (error) {
       return response.status(400).json({ error: "Verify your request data." });
