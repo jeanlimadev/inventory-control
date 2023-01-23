f21a86da6f38b664b96c70eff153250e6d11ecaa
Jean Lima <jeanlima.dev@gmail.com>
2023-01-23 11:40:57 -0300

    feat: list user profile

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 import { AuthenticateUserController } from "../controllers/users/AuthenticateUser/AuthenticateUserController";
 import { CreateUserController } from "../controllers/users/CreateUser/CreateUserController";
+import { ListUserProfileController } from "../controllers/users/ListUserProfile/ListUserProfileController";
 import { SendVerificationEmailController } from "../controllers/users/SendVerificationEmail/SendVerificationEmailController";
 import { VerifyUserEmailController } from "../controllers/users/VerifyUserEmail/VerifyUserEmailController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
@@ -11,6 +13,7 @@ const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const sendVerificationEmailController = new SendVerificationEmailController();
 const verifyUserEmailController = new VerifyUserEmailController();
+const listUserProfileController = new ListUserProfileController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -19,5 +22,7 @@ usersRoutes.post("/auth", authenticateUserController.handle);
 usersRoutes.post("/send-verify-mail", sendVerificationEmailController.handle);
 
 usersRoutes.get("/verify", verifyUserEmailController.handle);
+
+usersRoutes.get("/profile", ensureAuthenticated, listUserProfileController.handle);
 
 export { usersRoutes };
